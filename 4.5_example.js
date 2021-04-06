5791553aecbab3f7b84e89bac9832119c888f587
caitpozerski <caitlyn.m.pozerski@gmail.com>
2021-04-05 18:00:14 -0700

    first commit

---
+++ 4.5_example.js
@@ -0,0 +1,17 @@
+var firstVar = 3 // give a value to a variable, don't need a semicolon at end of line.
+var secondVar = 9 
+var firstArr = [5,6,7]
+console.log() // this is our print statements 
+console.log(firstArr[0]) // name of vairable first, then the index
+
+// Strings - a collection of characters. Container of characters
+
+var firstStr = "hello class!!!!";
+// console.log(firstStr[0])
+console.log(firstStr.charAt(3))
+
+
+// For Loops
+for(var i = 0; i < 4; i++){
+    console.log(i);
+}
